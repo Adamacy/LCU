1a41f098babf9a2af331bd45f8de230501c05094
Adamacy <a.jakubiak.srem@gmail.com>
2022-06-27 14:21:41 +0200

    Runes import. Runes copied to main file.

--- main.py
+++ main.py
@@ -9,11 +9,28 @@ from subprocess import check_output
 
 exceptions = Errors()
 
+convert = {
+    'The Adaptive Force Shard': 'Adaptive',
+    "The Attack Speed Shard": 'AttackSpeed',
+    'The Scaling CDR Shard': 'CDRScaling',
+    'The Armor Shard': "Armor",
+    "The Magic Resist Shard": "MagicRes",
+    "The Scaling Bonus Health Shard": "HealthScaling"
+}
+
+style = {
+    'Domination': 8100,
+    'Inspiration': 8300,
+    'Precision': 8000,
+    'Resolve': 8400,
+    'Sorcery': 8200
+}
 
 class Api:
 
     lang = 'en_US'
     champion = None
+    autoImport = False
 
     def __init__(self) -> None:
 
@@ -88,7 +105,7 @@ class Api:
         session = self.get('/lol-champ-select/v1/session').json()
         summoner = self.get('/lol-summoner/v1/current-summoner').json()
         champions = []
-        print(session)
+        
         self.puuid = summoner['puuid']
         self.summonerID = summoner['summonerId']
         self.accountID = summoner['accountId']
@@ -106,6 +123,15 @@ class Api:
                     champ = self.get(
                         f'/lol-champions/v1/inventories/{self.summonerID}/champions/{championID}')
                     champions.append(champ.json()['name'])
+                    for x in session['myTeam']:
+                        if summoner['summonerId'] == x['summonerId']:
+                            if self.autoImport == True:
+                                if x['championId'] == 0:
+                                    pass
+                                else:
+                                    champ = self.get(f"/lol-champions/v1/inventories/{self.summonerID}/champions/{x['championId']}")
+                                    self.setChampion(champ.json()['name'])
+                                    self.importRunes()
             return champions
 
     def getAllChampions(self):
@@ -175,12 +201,75 @@ class Api:
         return self.get('/lol-perks/v1/perks').json()
 
     def getChampBuild(self):
-        """Returns names of best build for selected champion"""
-        return runes.getRunes(self.champion)
+        """
+        Returns dict with best build for selected champion.
+        """
+
+        res = requests.get(f'https://u.gg/lol/champions/{self.champion}/build')
+        runes = []
+        
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        titles = soup.find_all(class_='perk-style-title')
+        primaryRune = titles[0].getText()
+        secondaryRune = titles[1].getText()
+
+        primary = soup.find(class_='rune-tree_v2 primary-tree')
+        primary = primary.find(class_='perk keystone perk-active')
+
+        primary = primary.find_all('img')[0]['alt']
+        
+        if "The Keystone" in primary:
+            runes.append(primary.replace("The Keystone ", ""))
+
+        rune = soup.find(class_='rune-tree_v2 primary-tree')
+        rune = rune.find_all(class_='perk-row')
+
+        for i in rune:
+            i = i.find(class_='perks')
+            i = i.find(class_='perk perk-active')
+            if i == None:
+                continue
+            data = i.find('img')['alt']
+            if "The Rune" in data:
+                rune = data.replace("The Rune ", "")
+                runes.append(rune)
+
+
+        secondary = soup.find(class_='secondary-tree')
+        cos = secondary.find_all(class_='perk-row')
+
+        for i in cos:
+            i = i.find(class_='perks')
+            i = i.find(class_='perk perk-active')
+            if i == None:
+                continue
+            data = i.find('img')['alt']
+            if "The Rune" in data:
+                rune = data.replace("The Rune ", "")
+                runes.append(rune)
+
+        stats = soup.find(class_='rune-tree_v2 stat-shards-container_v2')
+        statsAll = stats.find_all(class_='perks')
+        for i in statsAll:
+            i = i.find(class_='shard shard-active')
+            i = i.find('img')['alt']
+            runes.append(convert[i])
+        runesIds = json.loads(open('./runes.json', 'r').read())
+        runesConverted = []
+        for i in runes:
+            runesConverted.append(runesIds[i])
+
+        data = {
+            "primary": style[primaryRune],
+            "secondary": style[secondaryRune],
+            "ids": runesConverted,
+        }
+        
+        return data
 
     def importRunes(self):
-        """Doesn't Work"""
-
+        
         runes = self.getChampBuild()
         id = self.get('/lol-perks/v1/currentpage').json()['id']
         self.delete(f'/lol-perks/v1/pages/{id}')
